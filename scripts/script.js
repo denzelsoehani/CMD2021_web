116f313eddcd99ebc9783c8f428e006d3890fe1a
denzelsoehani <74145584+denzelsoehani@users.noreply.github.com>
2020-12-18 13:10:06 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,37 @@
 // JavaScript Document
+var button = document.getElementById("faq");
+var element = document.getElementById("toggle");
+
+element.classList.add("visible");
+
+button.addEventListener("click", function() {
+  element.classList.toggle("visible");
+});
+
+
+
+
+
+var button2 = document.getElementById("faq2");
+var element2 = document.getElementById("toggle2");
+
+element2.classList.add("visible");
+
+button2.addEventListener("click", function() {
+  element2.classList.toggle("visible");
+});
+
+
+
+
+
+var button3 = document.getElementById("faq3");
+var element3 = document.getElementById("toggle3");
+
+element3.classList.add("visible");
+
+button3.addEventListener("click", function() {
+  element3.classList.toggle("visible");
+});
+
+//https://css-tricks.com/video-screencasts/150-hey-designers-know-one-thing-javascript-recommend/
